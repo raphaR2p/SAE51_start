5e91c626140bf829b6fdeb81ed90cbf62e60f665
mamborap <raphael.mambo@univ-rouen.fr>
2025-09-11 09:56:35 +0200

    fini

--- argu_test.sh
+++ argu_test.sh
@@ -4,11 +4,11 @@ set +x
 # =========================
 # Variables
 # =========================
-RAM=4096
 DISK=64000      # en Mo
 TYPE_OS="Debian_64"
 ACTION=$1
 NAME=$2
+RAM=$3
 DIR="$HOME/VirtualBox VMs/$NAME"
 DISK_PATH="$DIR/$NAME.vdi"
 
@@ -22,35 +22,63 @@ case "$ACTION" in
 
     # --- Lister les machines ---
     L)
-        echo ">>> Liste des machines virtuelles :"
-        vboxmanage list vms
+                echo ">>> Liste des VMs :"
+        vboxmanage list vms | while read -r line; do
+            NAME_VM=$(echo "$line" | cut -d '"' -f 2)
+            echo "Machine : $NAME_VM"
+            vboxmanage getextradata "$NAME_VM" "CreatedOn" | sed 's/^.*Value: //'
+            vboxmanage getextradata "$NAME_VM" "Owner" | sed 's/^.*Value: //'
+			vboxmanage getextradata "$NAME_VM" "RAM" | sed 's/^.*Value: //'
+
+            echo "---------------------------"
+		done
         ;;
 
-    # --- Créer une nouvelle machine ---
+
+    # Créer nouvelle VM
     N)
-        if [ -z "" ]; then
-            echo "Erreur : vous devez fournir un nom de VM."
+    if [ -z "$NAME" ]; then
+        echo "Erreur : vous devez fournir un nom de VM."
+        exit 1
+    fi
+	
+	if [ -z "$RAM" ]; then
+            echo "Erreur : vous devez fournir la taille de la RAM (en Mo)."
             exit 1
-        fi
+    fi
 
-        if [ $? -ne 0 ]; then 
-    		echo "Erreur : impossible de créer la VM $NAME car elle existe déjà."
-   			exit 1
-		fi
+    if ! [[ "$RAM" =~ ^[0-9]+$ ]]; then
+            echo "Erreur : la RAM doit être un nombre (en Mo)."
+            exit 1
+    fi
 
-        echo ">>> Création de la VM $NAME..."
-        vboxmanage createvm --name "$NAME" --ostype "$TYPE_OS" --register
-        vboxmanage modifyvm "$NAME" --memory $RAM --nic1 nat --boot1 net
+    # (Vérifier si la VM existe déjà)
+    if vboxmanage list vms | grep -q "\"$NAME\""; then
+        echo "Erreur : la VM $NAME existe déjà."
+        exit 1
+    fi
 
-        mkdir -p "$DIR"
-        vboxmanage createmedium disk --filename "$DISK_PATH" --size $DISK
-        vboxmanage storagectl "$NAME" --name "SATA Controller" --add sata --controller IntelAhci
-        vboxmanage storageattach "$NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$DISK_PATH"
+    DIR="$HOME/VirtualBox VMs/$NAME"
+    DISK_PATH="$DIR/$NAME.vdi"
 
-        echo ">>> VM $NAME créée avec succès !"
-        ;;
+    echo ">>> Création de la VM $NAME..."
+    vboxmanage createvm --name "$NAME" --ostype "$TYPE_OS" --register
+    vboxmanage modifyvm "$NAME" --memory $RAM --nic1 nat --boot1 net
 
-    # --- Supprimer une machine ---
+    vboxmanage setextradata "$NAME" "CreatedOn" "$(date)"
+    vboxmanage setextradata "$NAME" "Owner" "$USER"
+	vboxmanage setextradata "$NAME" "RAM" "$RAM"
+
+
+    mkdir -p "$DIR"
+    vboxmanage createmedium disk --filename "$DISK_PATH" --size $DISK
+    vboxmanage storagectl "$NAME" --name "SATA Controller" --add sata --controller IntelAhci
+    vboxmanage storageattach "$NAME" --storagectl "SATA Controller" --port 0 --device 0 --type hdd --medium "$DISK_PATH"
+
+    echo ">>> VM $NAME créée avec succès !"
+    ;;
+
+    # Suppr
     S)
         if [ -z "$NAME" ]; then
             echo "Erreur : vous devez fournir le nom de la VM à supprimer."
@@ -66,7 +94,7 @@ case "$ACTION" in
         fi
         ;;
 
-    # --- Démarrer une machine ---
+    # Démarrer VM
     D)
         if [ -z "$NAME" ]; then
             echo "Erreur : vous devez fournir le nom de la VM à démarrer."
@@ -81,7 +109,7 @@ case "$ACTION" in
         fi
         ;;
 
-    # --- Arrêter une machine ---
+    # Arrêter VM
     A)
         if [ -z "$NAME" ]; then
             echo "Erreur : vous devez fournir le nom de la VM à arrêter."
@@ -95,6 +123,17 @@ case "$ACTION" in
         else
             echo "Erreur : la VM $NAME n'est pas en cours d'exécution."
         fi
+        ;;
+
+	# Suppr All
+    SA)
+        echo ">>> Suppression de toutes les machines virtuelles..."
+        vboxmanage list vms | while read -r line; do
+            NAME_VM=$(echo "$line" | cut -d '"' -f 2)
+            echo ">>> Suppression de la VM $NAME_VM..."
+            vboxmanage unregistervm "$NAME_VM" --delete
+        done
+        echo ">>> Toutes les machines ont été supprimées."
         ;;
 
 esac
